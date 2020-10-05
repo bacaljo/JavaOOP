d2bf60f5c8a594c550d7fed3728852b802c498b3
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-05 18:22:35 +0800

    Create an instance of Student in Main class and print out all its attributes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import model.Student;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Student john = new Student(
+                "John Emmanuel",
+                "Bacalla",
+                23,
+                "Male",
+                "2020M01");
+        System.out.println("First Name: " + john.getFirstName());
+        System.out.println("Last Name: " + john.getLastName());
+        System.out.println("Age: " + john.getAge());
+        System.out.println("Gender: " + john.getGender());
+        System.out.println("ID No: " + john.getIdNo());
     }
 }
